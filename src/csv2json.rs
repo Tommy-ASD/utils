99a5eb255f4eaefd2e0452e791d5e280fdf275b1
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2023-08-15 12:01:36 +0200

    no_panic

--- src/csv2json.rs
+++ src/csv2json.rs
@@ -1,12 +1,46 @@
 use csv::Reader;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use serde_json::Value;
+use serde_json::{json, Value};
 
-pub fn csv_to_json(mut csv: Reader<&[u8]>) -> serde_json::Value {
-    let headers = csv.headers().cloned().unwrap();
+use crate::error_types::TracebackError;
+
+/// This function takes in a csv::Reader<&[u8]> and returns a serde_json::Value
+/// It assumes that the first row of the csv is the header row, and that all
+/// other rows are data rows.
+/// It also assumes that all data is in the form of strings.
+/// Should these assumptions not be true, this function will return an error.
+/// NOTE: Some data will be lost in the conversion from csv to json.
+/// This happens because serde_json automatically sorts the CSV headers alphabetically.
+/// #PANICS
+/// This function should not panic under any circumstances, verified by no_panic.
+#[no_panic::no_panic]
+pub fn csv_to_json<T: std::io::Read>(
+    mut csv: Reader<T>,
+) -> Result<serde_json::Value, TracebackError> {
+    let headers = match csv.headers().cloned() {
+        Ok(headers) => headers,
+        Err(e) => {
+            return Err(TracebackError::new(
+                "Failed to read CSV headers".to_string(),
+                file!().to_string(),
+                line!(),
+            )
+            .with_extra_data(json!({ "error": e.to_string() })))
+        }
+    };
     let mut records = Vec::new();
     for result in csv.records() {
-        let record = result.unwrap();
+        let record = match result {
+            Ok(record) => record,
+            Err(e) => {
+                return Err(TracebackError::new(
+                    "Failed to read CSV record".to_string(),
+                    file!().to_string(),
+                    line!(),
+                )
+                .with_extra_data(json!({ "error": e.to_string() })))
+            }
+        };
         let mut obj = serde_json::Map::new();
         for (i, header) in headers.iter().enumerate() {
             obj.insert(
@@ -16,10 +50,10 @@ pub fn csv_to_json(mut csv: Reader<&[u8]>) -> serde_json::Value {
         }
         records.push(serde_json::Value::Object(obj));
     }
-    serde_json::Value::Array(records)
+    Ok(serde_json::Value::Array(records))
 }
 
-pub fn json_to_csv<'a>(json: Value) -> String {
+pub fn json_to_csv<'a>(json: Value) -> Result<String, TracebackError> {
     let mut wtr = csv::Writer::from_writer(vec![]);
     let headers = json[0].as_object().unwrap().keys();
     let mut collected_headers: Vec<String> = headers
@@ -35,7 +69,60 @@ pub fn json_to_csv<'a>(json: Value) -> String {
         }
         wtr.write_record(row).unwrap();
     }
-    String::from_utf8(wtr.into_inner().unwrap()).unwrap()
+    let inner = match wtr.into_inner() {
+        Ok(inner) => inner,
+        Err(e) => {
+            return Err(TracebackError::new(
+                "Failed to convert CSV writer to inner".to_string(),
+                file!().to_string(),
+                line!(),
+            )
+            .with_extra_data(json!({ "error": e.to_string() })))
+        }
+    };
+    match String::from_utf8(inner) {
+        Ok(string) => Ok(string),
+        Err(e) => {
+            return Err(TracebackError::new(
+                "Failed to convert CSV writer to string".to_string(),
+                file!().to_string(),
+                line!(),
+            )
+            .with_extra_data(json!({ "error": e.to_string() })))
+        }
+    }
+}
+
+/// This function takes in a csv file path and returns a serde_json::Value
+/// NOTE: Some data will be lost in the conversion from csv to json.
+/// This happens because serde_json automatically sorts the CSV headers alphabetically.
+pub fn csv_file_to_json(path: &str) -> Result<serde_json::Value, TracebackError> {
+    // read csv file, then pass it to csv_to_json
+    let rdr = match csv::Reader::from_path(path) {
+        Ok(rdr) => rdr,
+        Err(e) => {
+            return Err(TracebackError::new(
+                "Failed to read CSV file".to_string(),
+                file!().to_string(),
+                line!(),
+            )
+            .with_extra_data(json!({ "error": e.to_string() })))
+        }
+    };
+    match csv_to_json(rdr) {
+        Ok(json) => Ok(json),
+        Err(e) => Err(TracebackError::new(
+            "Failed to parse CSV to json".to_string(),
+            file!().to_string(),
+            line!(),
+        )
+        .with_parent(e)),
+    }
+}
+
+#[no_panic::no_panic]
+pub fn panics() {
+    panic!()
 }
 
 pub struct Person {
@@ -54,12 +141,15 @@ pub static BASIC_JSON: &str = r#"[{"name":"alice","age":"20"},{"name":"bob","age
 fn test_csv_to_json() {
     let csv = Reader::from_reader(BASIC_CSV.as_bytes());
     let json = csv_to_json(csv);
-    assert_eq!(json, serde_json::from_str::<Value>(BASIC_JSON).unwrap());
+    assert_eq!(
+        json.unwrap(),
+        serde_json::from_str::<Value>(BASIC_JSON).unwrap()
+    );
 }
 
 #[test]
 fn test_json_to_csv() {
     let json = serde_json::from_str::<Value>(BASIC_JSON).unwrap();
     let csv = json_to_csv(json);
-    assert_eq!(csv, BASIC_CSV);
+    assert_eq!(csv.unwrap(), BASIC_CSV);
 }
