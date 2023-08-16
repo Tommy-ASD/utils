9b15b5eca65f762355bc001f8c9bf878669dcb49
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2023-08-16 13:17:31 +0200

    removed error

--- src/error_types.rs
+++ src/error_types.rs
@@ -2,7 +2,6 @@ use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
 use std::{
-    collections::HashMap,
     error::Error,
     fmt::{Display, Formatter},
     fs::File,
