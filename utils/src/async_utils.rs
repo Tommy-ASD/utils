2271260618a0573adb00bb015307b4762a687207
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2023-09-04 09:06:24 +0200

    utils derive

--- utils/src/async_utils.rs
+++ utils/src/async_utils.rs
@@ -1,11 +1,9 @@
-use tokio::runtime::Runtime;
-
 // Function to run an asynchronous task synchronously using a shared Tokio runtime
 pub fn sync_execute<F, R>(task: F) -> R
 where
     F: std::future::Future<Output = R>,
 {
-    let rt = Runtime::new().unwrap();
+    let rt = tokio::runtime::Runtime::new().unwrap();
     rt.block_on(task)
 }
 
@@ -13,12 +11,13 @@ where
 macro_rules! sync_if_no_runtime {
     ($e:expr) => {{
         // Check if a runtime is already available
-        if let Ok(_) = Handle::try_current() {
+        if let Ok(_) = tokio::runtime::Handle::try_current() {
             // Spawn the task if a runtime is available
-            task::spawn(async move { $e.await });
+            // tokio::task::spawn(async move { $e.await })
+            $crate::async_utils::sync_execute($e)
         } else {
             // Run synchronously if no runtime is available
-            crate::async_utils::sync_execute($e)
+            $crate::async_utils::sync_execute($e)
         }
     }};
 }
