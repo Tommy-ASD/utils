256903c12408a5357a5edccb179d0c43b2ce86b3
Tommy <82094450+Tommy-ASD@users.noreply.github.com>
2023-09-08 10:08:00 +0200

    documentation + generate_schema

--- src/json.rs
+++ src/json.rs
@@ -5,6 +5,8 @@ use std::{
 
 use serde_json::{Map, Value};
 
+use crate::{error_types::TracebackError, traceback};
+
 /*
 This function is used to split the roller.json file into multiple files.
 The reason for this is that the file is too large to be uploaded to Surreal.
@@ -15,17 +17,18 @@ Parameters:
     split_size: usize - the size of the split files
 
 Returns:
-    None, as it only writes to files
+    Ok(()) if successful,
+    Err($crate::error_types::TracebackError) on failure
 
 Possible problems:
-    - if the file can not be read, the function will exit early
-    - if the file is malformed, the function will exit early
-    - if the JSON file is not an array, the function will exit early
+    - if the file can not be read, the function will return an error
+    - if the file is malformed, the function will return an error
+    - if the JSON file is not an array, the function will return an error
     - if writing to the files fails, it will not work
     - if the file is too large and the host machine doesn't have enough memory,
       i am not sure what will happen (probably a panic)
     - if the file is too large and the host machine doesn't have enough disk space,
-      i am also not sure what will happen (probably a panic)
+      i am also not sure what will happen (probably a panic, again)
 
 Possible improvements:
     - reduce code repetition
@@ -34,26 +37,23 @@ Possible improvements:
 Calls:
     - none
 */
-pub fn split_array_from_json_file(filepath: &str, split_size: usize) {
+pub fn split_array_from_json_file(filepath: &str, split_size: usize) -> Result<(), TracebackError> {
     let str = match read_to_string(filepath) {
         Ok(s) => s,
         Err(e) => {
-            println!("Error when reading roller JSON: {}", e);
-            return;
+            return Err(traceback!(e, "Error when reading roller JSON"));
         }
     };
     let parsed: serde_json::Value = match serde_json::from_str(&str) {
         Ok(p) => p,
         Err(e) => {
-            println!("Error when parsing roller JSON: {}", e);
-            return;
+            return Err(traceback!(e, "Error when parsing roller JSON"));
         }
     };
     let parsed = match parsed.as_array() {
         Some(p) => p,
         None => {
-            println!("Error when parsing roller JSON: not an array");
-            return;
+            return Err(traceback!("Error when parsing roller JSON: not an array"));
         }
     };
     let folder_path = filepath.split(".").collect::<Vec<&str>>()[filepath.split(".").count() - 2];
@@ -68,8 +68,7 @@ pub fn split_array_from_json_file(filepath: &str, split_size: usize) {
     match create_dir_all(format!(".{folder_path}")) {
         Ok(_) => {}
         Err(e) => {
-            println!("Error when creating directory: {}", e);
-            return;
+            return Err(traceback!(e, "Error when creating directory"));
         }
     };
     let mut i = 0;
@@ -78,26 +77,24 @@ pub fn split_array_from_json_file(filepath: &str, split_size: usize) {
         let mut file = match File::create(format!(".{folder_path}/{i}.{extension}")) {
             Ok(f) => f,
             Err(e) => {
-                println!("Error when creating file: {}", e);
-                return;
+                return Err(traceback!(e, "Error when creating file"));
             }
         };
         let chunk = match serde_json::to_string(chunk) {
             Ok(c) => c,
             Err(e) => {
-                println!("Error when parsing chunk: {}", e);
-                return;
+                return Err(traceback!(e, "Error when parsing chunk"));
             }
         };
         match file.write_all(chunk.as_bytes()) {
             Ok(_) => {}
             Err(e) => {
-                println!("Error when writing to file: {}", e);
-                return;
+                return Err(traceback!(e, "Error when writing to file"));
             }
         };
         i += 1;
     }
+    Ok(())
 }
 
 #[macro_export]
@@ -108,8 +105,13 @@ macro_rules! extract_nested_json {
             let parsed_to_type: $ret_type = match serde_json::from_value(j) {
                 Ok(v) => v,
                 Err(e) => return Err(
-                    format!(
-                        "Error when getting key {key} from json {json} with expected type {type}: {error}", key = $key, json = $json, type = stringify!($ret_type), error = e
+                    traceback!(e,
+                        format!(
+                            "Error when getting key {key} from json {json} with expected type {type}",
+                            key = $key,
+                            json = $json,
+                            type = stringify!($ret_type),
+                        )
                     )
                 ),
             };
@@ -145,4 +147,148 @@ pub fn detect_nested_json(json: &Map<String, Value>) -> Vec<String> {
         }
     }
     keys
+}
+
+/// Generates a JSON schema from a given JSON-like data structure.
+///
+/// This function takes a reference to a JSON-like data structure represented by
+/// the `serde_json::Value` enum and generates a JSON schema describing its structure.
+/// The schema describes the types of values, arrays, and objects contained in the input
+/// data, and their hierarchical relationships.
+///
+/// # Arguments
+///
+/// * `input`: A reference to the JSON-like data structure (`serde_json::Value`) for which
+///   you want to generate a schema.
+///
+/// # Returns
+///
+/// A `serde_json::Value` representing the JSON schema.
+///
+/// # Example
+///
+/// ```rust
+/// use serde_json::json;
+///
+/// let input_data = json!({
+///     "name": "John",
+///     "age": 30,
+///     "is_student": false,
+///     "hobbies": ["reading", "swimming"],
+/// });
+///
+/// let schema = generate_schema(&input_data);
+///
+/// // The `schema` now contains a JSON schema describing the structure of `input_data`.
+/// ```
+///
+/// # Note
+///
+/// This function recursively traverses the input data structure to generate the schema.
+/// It supports various data types including null, boolean, number, string, arrays, and objects.
+/// The generated schema is represented as a `serde_json::Value`.
+pub fn generate_schema(input: &serde_json::Value) -> serde_json::Value {
+    // Match the input value to determine its type and generate the schema accordingly
+    match input {
+        serde_json::Value::Null => serde_json::json!({"type": "null"}),
+        serde_json::Value::Bool(_) => serde_json::json!({"type": "boolean"}),
+        serde_json::Value::Number(_) => serde_json::json!({"type": "number"}),
+        serde_json::Value::String(_) => serde_json::json!({"type": "string"}),
+        serde_json::Value::Array(arr) => {
+            // Generate the schema for array values
+            let items_schema = arr.iter().fold(None, |schema, item| {
+                let item_schema = generate_schema(item);
+                match schema {
+                    Some(schema) => {
+                        if schema != item_schema {
+                            Some(serde_json::json!([schema, item_schema]))
+                        } else {
+                            Some(schema)
+                        }
+                    }
+                    None => Some(item_schema),
+                }
+            });
+
+            serde_json::json!({
+                "type": "array",
+                "items": items_schema.unwrap_or(serde_json::json!({}))
+            })
+        }
+        serde_json::Value::Object(obj) => {
+            // Generate the schema for object values
+            let properties: serde_json::Map<String, serde_json::Value> = obj
+                .iter()
+                .map(|(key, value)| {
+                    let prop_schema = generate_schema(value);
+                    (key.clone(), prop_schema)
+                })
+                .collect();
+
+            serde_json::json!({
+                "type": "object",
+                "properties": properties,
+                "required": obj.keys().cloned().collect::<Vec<String>>()
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    #[test]
+    fn test_generate_schema_null() {
+        let input = Value::Null;
+        let expected_schema = json!({"type": "null"});
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
+
+    #[test]
+    fn test_generate_schema_boolean() {
+        let input = Value::Bool(true);
+        let expected_schema = json!({"type": "boolean"});
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
+
+    #[test]
+    fn test_generate_schema_number() {
+        let input = Value::Number(serde_json::Number::from(42));
+        let expected_schema = json!({"type": "number"});
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
+
+    #[test]
+    fn test_generate_schema_string() {
+        let input = Value::String("Hello, World!".to_string());
+        let expected_schema = json!({"type": "string"});
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
+
+    #[test]
+    fn test_generate_schema_array() {
+        let input = json!([1, 2, 3]);
+        let expected_schema = json!({
+            "type": "array",
+            "items": {"type": "number"}
+        });
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
+
+    #[test]
+    fn test_generate_schema_object() {
+        let input = json!({"key1": 42, "key2": "value"});
+        let expected_schema = json!({
+            "type": "object",
+            "properties": {
+                "key1": {"type": "number"},
+                "key2": {"type": "string"}
+            },
+            "required": ["key1", "key2"]
+        });
+        assert_eq!(generate_schema(&input), expected_schema);
+    }
 }
